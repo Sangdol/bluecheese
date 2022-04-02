03389ea83a1d289facca267c82b5ff23e96b1834
Sanghyun Lee <hammerha@gmail.com>
2022-04-02 15:50:02 +0200

    Fix local-server tests

--- test/bluecheese/local_server_test.clj
+++ test/bluecheese/local_server_test.clj
@@ -2,17 +2,10 @@
   (:require [clojure.test :refer :all])
   (:require [bluecheese.local-server :refer :all]))
 
-
-(deftest read-md-as-html-test
-  (is (= "<!DOCTYPE html>\n<html lang=\"ko"
-         (subs (read-md-as-html "resources/md/kr/test/test-article.md")
-               0 30))))
-
-
 (deftest uri->path-test
   (is (= "resources/md/kr/blog/custom-static-site-generator.md"
-         (uri->path "/blog/2020/10/10/custom-static-site-generator")))
+         (uri->path "/blog/2020/10/10/custom-static-site-generator" (first env-configs))))
 
   (is (= "resources/md/kr/blog/fixed-about.md"
-         (uri->path "/about"))))
+         (uri->path "/about" (first env-configs)))))
 
